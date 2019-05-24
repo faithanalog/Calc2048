d05c17e3601fa8ca85d346825118bd662decbada
Faith Analog <faith@faithanalog.com>
2019-05-24 02:15:02 +0000

    lz4 version from 2016

--- compile.sh
+++ compile.sh
@@ -8,39 +8,9 @@ echo "     http://www.Cemetech.net"
 echo "----------------------------------"
 echo "----- Assembling $1 for the TI-83/84 Plus..."
 
-mkdir exec &> /dev/null
-mkdir list &> /dev/null
-
-echo "#define TI83P" >tasm/zztemp.asm
-echo ".binarymode TI8X" >>tasm/zztemp.asm
-
-output=$1
-output=${output%%.*}.8xp;
-if [ -e source/$1.asm ]
-then
-	cat source/$1.asm >>tasm/zztemp.asm
-elif [ -e source/$1.z80 ]
-then
-	cat source/$1.z80 >>tasm/zztemp.asm
-elif [ -e source/$1 ]
-then
-	cat source/$1 >>tasm/zztemp.asm
-else
-	echo "source/$1.asm, source/$1.z80, and source/$1 could be found."
-	exit 1
-fi
-if [ -z `which mono` ];
-then
-	echo "----- Mono is not installed! Install the mono-runtime package."
-	exit 1
-fi
-rm -f "exec/$1.bin"
-mono tasm/Brass.exe "tasm/zztemp.asm" "exec/$output" -l "list/$1.list.html"
-if [ -f "exec/$output" ];
-then
-	echo ----- $1 for the TI-83/84 Plus Assembled and Compiled.
-	echo TI-83 Plus version is $1.8xp
-else
-	echo "----- There were errors."
-fi
-rm -f tasm/zztemp.asm
+mono tasm/Brass.exe source/to_compress.z80 source/data.bin source/data_labels.inc
+lz4 -9 --no-frame-crc source/data.bin
+mono tasm/Brass.exe source/U2048.z80 exec/U2048.8xp -l list/U2048.list.html
+rm source/data.bin
+rm source/data.bin.lz4
+rm source/data_labels.inc
